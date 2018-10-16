79ef41e42cc49d4e250dfb2331c8142a00ad4918
ellie.ransom <ellie.ransom@splashthat.com>
2018-10-16 17:34:47 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+  return a + b
+}
+
+
+function subtract(a, b) {
+  return a - b
+}
+
+
+function multiply(a, b) {
+  return a*b
+}
+
+
+
+function divide() {
+
+}
+
+function inc(n)
